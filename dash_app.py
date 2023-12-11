73ef765fff99f27f56890dbf091ff4c1c856fc75
roborik <96759792+roborik@users.noreply.github.com>
2023-12-10 16:53:51 -0800

    final commit

--- dash_app.py
+++ dash_app.py
@@ -7,14 +7,13 @@ import plotly.graph_objs as go
 import pandas as pd
 import base64
 import io
-from esg_analysis import analyze_esg
 
-# Create Dash app
+# making app using dash
 app = Dash(__name__)
 
-# Layout of the app
+# app layout
 app.layout = html.Div(children=[
-    # Upload box at the top
+    # drag and drop csv upload
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -30,12 +29,14 @@ app.layout = html.Div(children=[
             'borderRadius': '5px',
             'textAlign': 'center',
             'margin': '10px',
-            'backgroundColor': 'lightgreen',  # Light green color
+            'backgroundColor': 'lightgreen',
         },
         multiple=False
     ),
 
-    # Dropdown menu for graph type
+    # Display box for uploaded file information
+    html.Div(id='upload-info-box', style={'margin': '10px', 'padding': '10px', 'border': '1px solid #ccc'}),
+
     html.Label("Select Graph Type:"),
     dcc.Dropdown(
         id='graph-type-dropdown',
@@ -43,28 +44,27 @@ app.layout = html.Div(children=[
             {'label': 'Candlestick', 'value': 'candlestick'},
             {'label': 'Line', 'value': 'line'}
         ],
-        value='candlestick',  # Default to candlestick
+        value='Line',  # default line since the majority of users are more familiar with this style of graph
         style={'width': '100%'}
     ),
 
-    # Rest of the app
     html.Label("Enter Stock Ticker:"),
     dcc.Input(id='stock-ticker-input', type='text', value='AAPL'),
     html.Button('Update Chart', id='update-button', n_clicks=0),
-    dcc.Graph(id='real-time-graph', style={'height': '80vh'}),  # Adjusted height
+    dcc.Graph(id='real-time-graph', style={'height': '80vh'}),
 ])
 
-# Function to update the candlestick or line chart
+# updates candlestick vs linechart
 @app.callback(Output('real-time-graph', 'figure'),
               [Input('update-button', 'n_clicks')],
               [State('stock-ticker-input', 'value'),
                State('graph-type-dropdown', 'value')])
 def update_graph(n_clicks, stock_symbol, graph_type):
     if n_clicks > 0:
-        # Fetch real-time stock data
+        # realtime stock data
         df = yf.download(stock_symbol, period="1d", interval="1m")
 
-        # Create chart based on selected type
+        # creates chart
         fig = make_subplots(rows=1, cols=1, subplot_titles=[f"Real-time Stock Price - {stock_symbol}"])
 
         if graph_type == 'candlestick':
@@ -82,16 +82,13 @@ def update_graph(n_clicks, stock_symbol, graph_type):
 
         fig.add_trace(trace)
 
-        # Update layout
         fig.update_layout(xaxis_rangeslider_visible=False)
 
         return fig
     else:
-        # Return an empty figure initially
         return go.Figure()
 
-# Callback to handle file upload
-@app.callback(Output('upload-data', 'children'),
+@app.callback(Output('upload-info-box', 'children'),
               [Input('upload-data', 'contents')],
               [State('upload-data', 'filename')])
 def upload_file(contents, filename):
@@ -103,23 +100,36 @@ def upload_file(contents, filename):
     decoded = base64.b64decode(content_string)
     df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
 
-    # Output a list of unique values in columns containing 'tick' or 'symb'
+    # looks for column containing 'tick' or 'symb'
     tick_symb_columns = [col for col in df.columns if 'tick' in col.lower() or 'symb' in col.lower()]
     unique_values = []
 
+    info_text = []
     for column in tick_symb_columns:
         unique_values.extend(df[column].unique())
     unique_values = list(set(unique_values))
-    print(unique_values)
 
-    # for i in unique_values:
-    #     lambda
+    data = pd.read_csv('SP 500 ESG Risk Ratings.csv')
+    for i in unique_values:
+        def get_esg_data(ticker):
+            filtered_data = data[data['Symbol'] == ticker]
+            if filtered_data.empty:
+                return "Ticker not found"
+            esg_data = filtered_data[['Total ESG Risk score', 'Environment Risk Score', 'Governance Risk Score',
+                                      'Social Risk Score', 'Controversy Level']].values.tolist()
+            return esg_data
 
+        ticker_input = i
+        result = get_esg_data(ticker_input)
 
+        if result == "Ticker not found":
+            info_text.append(result)
+        else:
+            label = f'{i}, Total ESG Risk score'
+            info_text.append(label + ": " + str(result[0][0]))
 
+    return info_text
 
-    return f'file {filename} uploaded successfully!'
-
-# Run the app
+# runs app
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
